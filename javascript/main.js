2ee2f4db149c5c0404587a1011ceef7131790260
junilowahab <junilojoshuawahab@gmail.com>
2023-10-13 15:09:56 +0100

    implemented 'display movie details' on hover effect

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,22 @@
 import netflix from '../neflix-titles/netflix-titles.json' assert {type:'json'};
+let thumbnailTitle = document.createElement('h1');
+let thumbnailDescription = document.createElement('p');
+
+let thumbnails = document.querySelectorAll('.movie-thumbnails');
+thumbnails.forEach(thumbnail => {
+
+    //Add movie details on hover
+    thumbnail.addEventListener('mouseover', () => {
+        let thumbnailID = thumbnail.getAttribute('id');
+        function findMovieDetails(x){
+            thumbnailTitle.innerText = netflix['series'][thumbnailID]['title'];
+        };
+        findMovieDetails(thumbnailID);
+        thumbnail.appendChild(thumbnailTitle);
+    });
+
+    //Remover movie details when moude leaves
+    thumbnail.addEventListener('mouseleave', () => {
+        thumbnail.removeChild(thumbnailTitle)
+    })
+})
