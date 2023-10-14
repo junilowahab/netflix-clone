8aac27181bd2a36ec1a2e388543cc445b3e6a6e0
junilowahab <junilojoshuawahab@gmail.com>
2023-10-14 18:26:53 +0100

    Implemented image change while netflix thumbnail is hovered

--- javascript/main.js
+++ javascript/main.js
@@ -10,15 +10,19 @@ thumbnails.forEach(thumbnail => {
         let thumbnailID = thumbnail.getAttribute('id');
         findMovieDetails(thumbnailID);
         thumbnail.append(thumbnailTitle, thumbnailDescription);
+        thumbnail.style.backgroundImage = `url(../images/${netflix['series'][thumbnailID]['hoverImage']})`
     });
 
     //Remover movie details when mouse leaves
     thumbnail.addEventListener('mouseleave', () => {
+        let thumbnailID = thumbnail.getAttribute('id');
         thumbnail.removeChild(thumbnailTitle);
         thumbnail.removeChild(thumbnailDescription);
+        thumbnail.style.backgroundImage = `url(../images/${netflix['series'][thumbnailID]['originalImage']})`
     })
 });
 
+//Function to locate the movie details using the id
 function findMovieDetails(id){
     thumbnailTitle.innerText = netflix['series'][id]['title'];
     thumbnailDescription.innerText = netflix['series'][id]['description'];
